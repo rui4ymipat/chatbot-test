cef56f3ee6dd5b11f72840a9eed1eddae508ea46
rui4ymipat <Peerapatpatty@gmail.com>
2020-07-30 13:59:16 +0700

    C

--- chatbot13349/app.py
+++ chatbot13349/app.py
@@ -1,39 +1,75 @@
-from flask import Flask, request
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
 import json
-import requests
+import numpy as np
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
 app = Flask(__name__)
+
+lineaccesstoken = 'tRzwQrc6+69acOH45UBSDAOPhq7fDHloUw20DcLdNsA1N+tJyc8Z9ZOgaN2liOL8QRCjS0AoJ9eEFP7UYBGAJiMP44zUWLdKHS+pVN0lnkiaUmmDURLmVm6nUoU5JNAtayYiC0nuiBpV384F6nUZ2AdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
+
+####################### new ########################
 @app.route('/')
 def index():
-  return "Hello World!"
+    return "Hello World!"
 
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
+
+@app.route('/webhook', methods=['POST'])
 def callback():
-  json_line = request.get_json()
-  json_line = json.dumps(json_line)
-  decoded = json.loads(json_line)
-  user = decoded["events"][0]['replyToken']
-  #id=[d['replyToken'] for d in user][0]
-  #print(json_line)
-  print("ผู้ใช้：",user)
-  sendText(user,'งง') # ส่งข้อความ งง
-  return '',200
+    json_line = request.get_json(force=False,cache=False)
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    no_event = len(decoded['events'])
+    for i in range(no_event):
+        event = decoded['events'][i]
+        event_handle(event)
+    return '',200
 
 
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  Authorization = 'tRzwQrc6+69acOH45UBSDAOPhq7fDHloUw20DcLdNsA1N+tJyc8Z9ZOgaN2liOL8QRCjS0AoJ9eEFP7UYBGAJiMP44zUWLdKHS+pVN0lnkiaUmmDURLmVm6nUoU5JNAtayYiC0nuiBpV384F6nUZ2AdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-  headers = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Authorization':Authorization
-  }
-  data = json.dumps({
-  "replyToken":user,
-  "messages":[{"type":"text","text":text}]})
-  #print("ข้อมูล：",data)
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-  #print(r.text)
+def event_handle(event):
+    print(event)
+    try:
+        userId = event['source']['userId']
+    except:
+        print('error cannot get userId')
+        return ''
 
+    try:
+        rtoken = event['replyToken']
+    except:
+        print('error cannot get rtoken')
+        return ''
+    try:
+        msgId = event["message"]["id"]
+        msgType = event["message"]["type"]
+    except:
+        print('error cannot get msgID, and msgType')
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+        return ''
+
+    if msgType == "text":
+        msg = str(event["message"]["text"])
+        replyObj = TextSendMessage(text=msg)
+        line_bot_api.reply_message(rtoken, replyObj)
+
+    else:
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+    return ''
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
